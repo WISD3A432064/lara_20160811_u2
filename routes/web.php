a93904dc9fbab817761b0680d16142c51daab94c
nini <poikl15397@gmail.com>
2017-10-15 15:02:22 +0800

    設定 Route 前置

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,13 @@ Route::get('hello/{name?}',function($name='Everybody'){
 Route::get('say/{name?}',['as'=>'hello.index',function($name='Everybody'){
     return'Hello,'.$name;
 }]);
+
+Route::get('dashboard',function(){
+    return'dashboard';
+});
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admindashboard';
+    });
+});
